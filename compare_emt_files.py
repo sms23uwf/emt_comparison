8e6bb1d6c64c8b90fda4989e2acc8cbf88a823a8
Steve Satterfield <stevesatterfield2286@gmail.com>
2020-08-11 20:01:40 -0500

    daily modifications

--- compare_emt_files.py
+++ compare_emt_files.py
@@ -20,7 +20,7 @@ from generator import Generator
 
 
 
-writeFile = 'diff4.txt'
+writeFile = 'diff.txt'
 baseFileName = 'SPECTRE_OUTPUT_07032020.emt'
 comparisonFileName = 'SPECTRE_OUTPUT_08032020.emt'
   
@@ -189,22 +189,17 @@ def parseComparisonFile():
             
                     
 def findElnot(elnotValue, inArray):
-    print("elnotValue: {}, inArray: {}".format(elnotValue, inArray))
     if inArray == constant.COMPARISON_ARRAY:
         for emitter in comparison_emitters:
             if emitter.get_elnot() == elnotValue:
-                print("just about to return emitter for elnot: {}".format(elnotValue))
                 return emitter
         
-        print("just about to return empty array for search for elnot {}".format(elnotValue))
         return []
     else:
         for emitter in base_emitters:
             if emitter.get_elnot() == elnotValue:
-                print("just about to return emitter for elnot: {}".format(elnotValue))
                 return emitter
             
-        print("just about to return empty array for search for elnot {}".format(elnotValue))
         return []
 
 
@@ -212,9 +207,18 @@ def findEmitterMode(emitterModeName, comparisonEmitter):
         for comparisonEmitterMode in comparisonEmitter.get_modes():
             if comparisonEmitterMode.get_name() == emitterModeName:
                 return comparisonEmitterMode
-            
+          
         return []
     
+
+def findGenerator(generatorNumber, comparisonEmitterMode):
+        for comparisonGenerator in comparisonEmitterMode.get_generators():
+            if comparisonGenerator.get_generator_number() == generatorNumber:
+                print("just about to return generator: {} as match for generator: {}".format(comparisonGenerator.get_generator_number(), generatorNumber))
+                return comparisonGenerator
+          
+        return []
+
 
 def findAttribute(baseAttributeName, comparisonEmitter):
     for comparisonAttribute in comparisonEmitter.get_attributes():
@@ -225,61 +229,68 @@ def findAttribute(baseAttributeName, comparisonEmitter):
 
         
     
-def compareEMTFiles():
-    wf = open(writeFile, "w+")
-    
-    print("parsing Base File ...")
-    parseBaseFile()
-    
-    print("parsing Comparison File ...")
-    parseComparisonFile()
-    
-    for baseEmitter in base_emitters:
+def compareEMTFiles(wf, base_emitter_collection, comparison_emitter_collection, comparisonArray, bFileName, cFileName):
+     
+    for baseEmitter in base_emitter_collection:
         bElnot = baseEmitter.get_elnot()
-        comparisonEmitter = findElnot(bElnot, constant.COMPARISON_ARRAY)
+        comparisonEmitter = findElnot(bElnot, comparisonArray)
         
         if not comparisonEmitter:
-            wf.write("Base file contains emitter with elnot: {} that is not found in Comparison file.\n".format(bElnot))
+            wf.write("{} contains emitter with elnot: {} that is not found in {}.\n".format(bFileName, bElnot, cFileName))
         else:
             for baseAttribute in baseEmitter.get_attributes():
                 #print("baseAttribute: {} = {}".format(baseAttribute.get_name(), baseAttribute.get_value()))
                 comparisonAttribute = findAttribute(baseAttribute.get_name(), comparisonEmitter)
                 
                 if not comparisonAttribute:
-                    wf.write("Base file contains emitter {} with attribute {} that is missing from Comparison file.\n".format(bElnot, baseAttribute.get_name()))
-                
+                    wf.write("{} contains emitter {} with attribute {} that is missing from {}.\n".format(bFileName, bElnot, baseAttribute.get_name(), cFileName))
+                else:
+                    if comparisonAttribute.get_value() != baseAttribute.get_value():
+                        wf.write("{} emitter {} contains attribute: {} with value: {} which is different from {} attribute value of {}.\n".format(bFileName, bElnot, baseAttribute.get_name(), baseAttribute.get_value(), cFileName, comparisonAttribute.get_value()))
+                        
             for baseEmitterMode in baseEmitter.get_modes():
-                comparisonEmitterMode = findEmitterMode(baseEmitterMode.get_name(), baseEmitter)
+                comparisonEmitterMode = findEmitterMode(baseEmitterMode.get_name(), comparisonEmitter)
                 
                 if not comparisonEmitterMode:
-                    wf.write("Base file emitter {} contains emitterMode {} that is missing from Comparison file.\n".format(bElnot, baseEmitterMode.get_name()))
-                
-     
-    for comparisonEmitter in comparison_emitters:
-        cElnot = comparisonEmitter.get_elnot()
-        baseEmitter = findElnot(cElnot, constant.BASE_ARRAY)
+                    wf.write("{} emitter {} contains emitterMode {} that is missing from {}.\n".format(bFileName, bElnot, baseEmitterMode.get_name(), cFileName))
+                else:
+                    for baseModeAttribute in baseEmitterMode.get_attributes():
+                        comparisonModeAttribute = findAttribute(baseModeAttribute.get_name(), comparisonEmitterMode)
+                        
+                        if not comparisonModeAttribute:
+                            wf.write("{} contains emitter mode {} with attribute {} that is missing from the emitter mode in {}.\n".format(bFileName, baseEmitterMode.get_name(), baseModeAttribute.get_name(), cFileName))
+                        else:
+                            if comparisonModeAttribute.get_value() != baseModeAttribute.get_value():
+                                wf.write("{} emitter mode {} contains attribute {} with value: {} which is different from {} attribute value of {}.\n".format(bFileName, baseEmitterMode.get_name(), baseModeAttribute.get_name(), baseModeAttribute.get_value(), cFileName, comparisonModeAttribute.get_value()))
+                                
+                    for baseGenerator in baseEmitterMode.get_generators():
+                        comparisonGenerator = findGenerator(baseGenerator.get_generator_number(), comparisonEmitterMode)
+                    
+                    if not comparisonGenerator:
+                        wf.write("{} mode {} contains generator: {} that is missing from the mode in {}".format(bFileName, baseEmitterMode.get_name(), baseGenerator.get_generator_number(), cFileName))
+                        
+                                
+                            
+ 
+def compareTheFiles():
 
-        if not baseEmitter:
-            wf.write("Comparison file contains emitter with elnot: {} that is not found in Base file.\n".format(cElnot))
-        else:
-            for comparisonAttribute in comparisonEmitter.get_attributes():
-                #print("comparisonAttribute: {} = {}".format(comparisonAttribute.get_name(), comparisonAttribute.get_value()))
-                baseAttribute = findAttribute(comparisonAttribute.get_name(), baseEmitter)
-                
-                if not baseAttribute:
-                    wf.write("Comparison file contains emitter {} with attribute {} that is missing from Base file.\n".format(cElnot, comparisonAttribute.get_name()))
-                    
-            for comparisonEmitterMode in comparisonEmitter.get_modes():
-                baseEmitterMode = findEmitterMode(comparisonEmitterMode.get_name(), comparisonEmitter)
-                
-                if not baseEmitterMode:
-                    wf.write("Comparison file emitter {} contains emitterMode {} that is missing from Base file.\n".format(cElnot, comparisonEmitterMode.get_name()))
-                    
-        
+    print("parsing Base File ...")
+    parseBaseFile()
+    
+    print("parsing Comparison File ...")
+    parseComparisonFile()
+
+    wf = open(writeFile, "w+")
+
+    compareEMTFiles(wf, base_emitters, comparison_emitters, constant.COMPARISON_ARRAY, baseFileName, comparisonFileName)
+
+    compareEMTFiles(wf, comparison_emitters, base_emitters, constant.BASE_ARRAY, comparisonFileName, baseFileName)
+
     wf.close()
-                   
+
+           
 if __name__ == '__main__':
-    compareEMTFiles()
+    compareTheFiles()
     
                     
                 
